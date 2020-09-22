9a99f6604b481847bd781c451ab9826aa9043a13
Markus Ressel <markus9100@gmail.com>
2020-09-23 00:06:44 +0200

    reworked file locks

--- cpulimit-daemon.sh
+++ cpulimit-daemon.sh
@@ -6,8 +6,10 @@ trap '
   # wait for limit loops to exit
   color_echo "${Red}" "Shutting down..."
   wait
-  rm "$LIMITED_PIDS_PATH"
-  rm "$WORKER_PROCESSES_PATH"
+  rm -f "$LIMITED_PIDS_PATH"
+  rm -f "$DB_LOCK"
+  rm -f "$WORKER_PROCESSES_PATH"
+  rm -f "$WORKER_LOCK"
   kill -s INT "$$"
 ' INT
 
@@ -28,7 +30,7 @@ fi
 # TODO: does not work because pgrep exists with 1 if no process was found
 #set -e
 # exit on undeclared variable
-set -u
+# set -u
 
 ## Colors for console output
 # Reset
@@ -73,6 +75,26 @@ if [ -z "$searchterm" ]; then
   exit 1
 fi
 
+declare -A LOCK_FDS=()                        # store FDs in an associative array
+getLock() {
+  local file=$(readlink -f "$1")              # declare locals; canonicalize name
+  local op=$2
+  case $op in
+    LOCK_UN)
+      [[ ${LOCK_FDS[$file]} ]] || return      # if not locked, do nothing
+      eval "exec ${LOCK_FDS[$file]}>&-"       # close the FD, releasing the lock
+      unset LOCK_FDS[$file]                   # ...and clear the map entry.
+      ;;
+    LOCK_EX)
+      [[ ${LOCK_FDS[$file]} ]] && return      # if already locked, do nothing
+      local new_lock_fd                       # don't leak this variable
+      exec {new_lock_fd}>"$file"              # open the file...
+      flock -x "$new_lock_fd"                 # ...lock the fd...
+      LOCK_FDS[$file]=$new_lock_fd            # ...and store the locked FD.
+      ;;
+  esac
+}
+
 function run_limit_loop() {
   local pid=$1
 
@@ -86,20 +108,15 @@ function run_limit_loop() {
 
   ### remove pid from "database" file
   # aquire file lock
-  exec {lock_fd}>"$DB_LOCK" || exit 1
-  flock "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
+  getLock "$DB_LOCK" "LOCK_EX"
     sed -i "/$pid/d" "$LIMITED_PIDS_PATH"
-  # release lock
-  flock -u "$lock_fd"
+  getLock "$DB_LOCK" "LOCK_UN"
 
   ### remove ourselfs from the "worker" list
-  # aquire file lock
-  exec {lock_fd}>"$WORKER_LOCK" || exit 1
-  flock "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
+  getLock "$WORKER_LOCK" "LOCK_EX"
     this_pid="$$"
     sed -i "/$this_pid/d" "$WORKER_PROCESSES_PATH"
-  # release lock
-  flock -u "$lock_fd"
+  getLock "$WORKER_LOCK" "LOCK_UN"
 }
 
 function contains() {
@@ -140,47 +157,44 @@ count=0
 while :
 do
   # read list of currently limited processes
-  exec {lock_fd}>"$DB_LOCK" || exit 1
-  flock "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
+  getLock "$DB_LOCK" "LOCK_EX"
     readarray -t limited_pids < "$LIMITED_PIDS_PATH"
-  flock -u "$lock_fd"
-
-  # Find process ids to limit
-  pids=
-  if [ -z "$f_flag" ]; then
-    pids=$(pgrep    -a "$searchterm" | grep -vf "$WORKER_PROCESSES_PATH" | grep -vE ".*bash .*cpulimit-daemon.*" | awk '{print $1}')
-  else
-    pids=$(pgrep -f -a "$searchterm" | grep -vf "$WORKER_PROCESSES_PATH" | grep -vE ".*bash .*cpulimit-daemon.*" | awk '{print $1}')
-  fi
-
-  new_count=$(echo "$pids" | wc -w)
-  if [[ "$new_count" != "$count" ]]; then
-    count=$new_count
-    color_echo "${Yellow}" "$count matching process(es)"
-  fi
   
-  # for each pid (that is not yet limited) run a limit loop in the background
-  for pid in $pids
-  do
-    if [ "$(contains "${limited_pids[@]}" "$pid")" == "n" ]; then
-      # our "database" does not contain the pid yet
-      
-      # write pid to our "database" file
-      exec {lock_fd}>"$DB_LOCK" || exit 1
-      flock "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
-        echo -e "${pid}" >> "$LIMITED_PIDS_PATH"
-      flock -u "$lock_fd"
-
-      run_limit_loop "$pid" &
-
-      # memorize pid of the "limiter" process
-      exec {lock_fd}>"$WORKER_LOCK" || exit 1
-      flock "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
-        echo -e "$!" >> "$WORKER_PROCESSES_PATH"
-      flock -u "$lock_fd"
+    # Find process ids to limit
+    pids=
+    if [ -z "$f_flag" ]; then
+      pids=$(pgrep    -a "$searchterm" | grep -vf "$WORKER_PROCESSES_PATH" | grep -vE ".*bash .*cpulimit-daemon.*" | awk '{print $1}')
+    else
+      pids=$(pgrep -f -a "$searchterm" | grep -vf "$WORKER_PROCESSES_PATH" | grep -vE ".*bash .*cpulimit-daemon.*" | awk '{print $1}')
     fi
-  done
+
+    new_count=$(echo "$pids" | wc -w)
+    if [[ "$new_count" != "$count" ]]; then
+      count=$new_count
+      color_echo "${Yellow}" "$count matching process(es)"
+    fi
+    
+    # for each pid (that is not yet limited) run a limit loop in the background
+    for pid in $pids
+    do
+      if [ "$(contains "${limited_pids[@]}" "$pid")" == "n" ]; then
+        # our "database" does not contain the pid yet
+        
+        # write pid to our "database" file
+        echo -e "${pid}" >> "$LIMITED_PIDS_PATH"
+
+        run_limit_loop "$pid" &
+
+        # memorize pid of the "limiter" process
+        getLock "$WORKER_LOCK" "LOCK_EX"
+          echo -e "$!" >> "$WORKER_PROCESSES_PATH"
+        getLock "$WORKER_LOCK" "LOCK_UN"
+      fi
+
+    done
+
+  getLock "$DB_LOCK" "LOCK_UN"
 
   # sleep before checking for new processes
-  sleep 0.2
+  sleep 0.1
 done
